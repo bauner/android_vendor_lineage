82821883218db439a3b12a82d6fa133cef820597
Dominggoes Isakh <drjisakh@gmail.com>
2017-09-03 19:14:17 +0200

    soong_config: Add TARGET_NEEDS_EXYNOS4_ENHANCEMENTS
     * Soong product variable 'Exynos4_enhancements'
    
    Change-Id: Ied29417e9e1ecc424d99f452d7879bd2e723a1cc

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,8 +1,12 @@
 package android
 type Product_variables struct {
+	Exynos4_enhancements struct {
+		Cflags []string
+		Cppflags []string
+	}
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
-	}
+        }
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
@@ -25,6 +29,7 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
+	Exynos4_enhancements    *bool `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
